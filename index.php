555cb28b58323eb19ddcae48ea3eac9fca84c4bf
icoooh <victorhugosalomao2009@gmail.com>
2026-04-15 14:29:19 -0300

    commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $owner = "Victor Hugo Salomão";
+    $contributor = null
+?>
+
+<h1>Pull Request em Dupla</h1>
+
+<?php
+    echo '<h2 style="color:cyan">Dono: ',$owner.'</h2>'
+?>
+
+<h2>Contribuinte:
+
+<?php
+    echo $contributor
+?>
+
+</h2>
